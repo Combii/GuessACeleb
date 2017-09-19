1c473d2972b2ed22b479c6b7ffbffe0fa78b16a1
Combii <davidstovle@gmail.com>
2017-09-19 13:26:57 +0200

    New stuff

--- app/src/main/java/com/combii/guessaceleb/MainActivity.java
+++ app/src/main/java/com/combii/guessaceleb/MainActivity.java
@@ -3,20 +3,34 @@ package com.combii.guessaceleb;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//:0)
+        setContentView(R.layout.activity_main);
     }
+
+
+
 
 
     public Map<String, String> findCelebs(String html) {
 
+        Map<String,String> celebs = new HashMap<>();
 
+        Pattern p = Pattern.compile("<img src=\"(.*?)\" alt=\"(.*?)\"");
+
+        Matcher m = p.matcher(html);
+
+        while(m.find()) {
+            System.out.println(m.group(1) + " " + m.group(2));
+        }
 
     }
 
