15e0b3088839a29f748809a53daca92fcf3583ef
Combii <davidstovle@gmail.com>
2017-09-19 13:25:43 +0200

    Stuff

---
+++ app/src/main/java/com/combii/guessaceleb/MainActivity.java
@@ -0,0 +1,23 @@
+package com.combii.guessaceleb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//:0)
+    }
+
+
+    public Map<String, String> findCelebs(String html) {
+
+
+
+    }
+
+}
